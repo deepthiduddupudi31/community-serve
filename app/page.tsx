c9906e92b99ff2914414c5300b70907d8f70b3ea
FAIZ KHAN <nexusblade5195@gmail.com>
2025-07-27 17:06:02 +0530

    Deplyed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import Input from "../frontend/src/components/ui/input"
+import {Input} from "../frontend/src/components/ui/input"
 
 export default function SyntheticV0PageForDeployment() {
-  return < Input/>
+  return <Input/>
 }
