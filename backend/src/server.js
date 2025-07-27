25acb0ae34d1afc30b003207c4fcb0ab06049a17
FAIZ KHAN <nexusblade5195@gmail.com>
2025-07-27 17:17:42 +0530

    Deplyed

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,8 +16,9 @@ connectDB()
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: "https://community-serve-pxj8lucyr-duddupudideepthi31-gmailcoms-projects.vercel.app" || "http://localhost:3000",
     credentials: true,
+    
   }),
 )
 app.use(express.json({ limit: "10mb" }))
